397c91236be0e1d41296caf3a67d9dae86a17620
Adnan <adnan@websemantics.ca>
2016-04-29 08:46:50 -0400

    Fixes #27, add Travis support, move Change log into own file

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 ```
   ___                              ____       _           _   
- |_ _|_ __ ___   __ _  __ _  ___  / ___|  ___| | ___  ___| |_ 
+ |_ _|_ __ ___   __ _  __ _  ___  / ___|  ___| | ___  ___| |_
   | || '_ ` _ \ / _` |/ _` |/ _ \ \___ \ / _ \ |/ _ \/ __| __|
-  | || | | | | | (_| | (_| |  __/  ___) |  __/ |  __/ (__| |_ 
+  | || | | | | | (_| | (_| |  __/  ___) |  __/ |  __/ (__| |_
  |___|_| |_| |_|\__,_|\__, |\___| |____/ \___|_|\___|\___|\__|
-                      |___/  v1.5
+                      |___/  v1.6
 ```
-> Last update: 5 March 2016
 
-We designed this plugin extension as a humanized UI element for social networking sites that need to faciliate relations between people. Research shows that people are extremely sensitvie to photos of others, so we needed to revamp the traditional UI elements to make them more intuitive and human.
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/websemantics/Image-Select/master/LICENSE) [![GitHub forks](https://img.shields.io/github/forks/websemantics/Image-Select.svg)](https://github.com/websemantics/Image-Select/network) [![GitHub stars](https://img.shields.io/github/stars/websemantics/Image-Select.svg)](https://github.com/websemantics/Image-Select/stargazers)
+[![Percentage of issues still open](http://isitmaintained.com/badge/open/websemantics/Image-Select.svg)](http://isitmaintained.com/project/websemantics/Image-Select "Percentage of issues still open")
 
-You can use these plugin extensions for modelling multiple (one-to-many) or single (one-to-one) relations between people. Go to [main page](http://websemantics.github.io/Image-Select/) or try this [example](http://websemantics.github.io/Image-Select/example.html).
+> We designed this plugin extension as a humanized UI element for social networking sites that need to faciliate relations between people. Research shows that people are extremely sensitvie to photos of others, so we needed to revamp the traditional UI elements to make them more intuitive and human.
 
 ![Alt text](http://websemantics.github.io/Image-Select/img/screen-shot.png "Image Select")
+
+
+## Use Scenarios
+
+You can use these plugin extensions for modelling multiple (one-to-many) or single (one-to-one) relations between people. Go to [main page](http://websemantics.github.io/Image-Select/) or try this [example](http://websemantics.github.io/Image-Select/example.html).
 
 We couldn't find any scripts that had this full functionality, so we developed it ourselves on top of Chosen. Hope you find it helpful, and get back if you have any feedback/improvements.
 
@@ -22,7 +27,7 @@ We couldn't find any scripts that had this full functionality, so we developed i
 - Install dependencies, `bower install`
 - Browse to `index.html` or `example.html`
 
-### Via Bower 
+### Via Bower
 
 `bower install image-select`
 
@@ -35,38 +40,16 @@ To get information about available packages
 You only need to add a `data-img-src` attribute to your `<option>` tag.
 ```HTML
 <select class="my-select">
-  <option data-img-src="img/adnan.png">Adnan Sagar</option> 
-  <option data-img-src="img/rena.png">Rena Cugelman</option> 
-  <option data-img-src="img/tavis.png">Tavis Lochhead</option> 
-  <option data-img-src="img/brian.png" selected="selected">Brain Cugelman</option> 
+  <option data-img-src="img/adnan.png">Adnan Sagar</option>
+  <option data-img-src="img/rena.png">Rena Cugelman</option>
+  <option data-img-src="img/tavis.png">Tavis Lochhead</option>
+  <option data-img-src="img/brian.png" selected="selected">Brain Cugelman</option>
 </select>
 ```
 Then call Chosen as you would with the options you need.
 ```JAVASCRIPT
 $(".my-select").chosen();
 ```
-
-
-# Change Log
-All notable changes to this project will be documented in this section.
-
-## [1.5] - 2016-02-08
-### Changed
-- Flexible templates (insert images before / after text regardless of rtl)
-- Various code fixes
-
-### [1.4] - 2015-08-18
-#### Changed
-- Use [Bower](http://bower.io/) for dependancies.
-- Merged main project and `gh-pages` into one codebase.
-- Upgrade to latest [Chosen](http://github.com/harvesthq/chosen) library.
-- Support for Left to Right, LTR (i.e. Arabic).
-- Code refactoring to improve quality and ease of contribution.
-- Fixed all known issues
-
-### [1.3] - 2015-03-29
-#### Changed
-- Fixed few bugs.
 
 # Used by
 
--- dist/ImageSelect.jquery.js
+++ dist/ImageSelect.jquery.js
@@ -1,12 +1,12 @@
 /**
  * Image Select (Version 1.4)
  *
- * Image Select is an extention to the Chosen, a Select Box Enhancer for 
+ * Image Select is an extention to the Chosen, a Select Box Enhancer for
  * jQuery and Prototype, full source at https://github.com/harvesthq/chosen
  *
- * This plugin extension was designed as a humanized UI element for social networking 
- * sites that need to faciliate relations between people. Research shows that people 
- * are extremely sensitvie to photos of others, so we needed to revamp the traditional 
+ * This plugin extension was designed as a humanized UI element for social networking
+ * sites that need to faciliate relations between people. Research shows that people
+ * are extremely sensitvie to photos of others, so we needed to revamp the traditional
  * UI elements to make them more intuitive and human.
  *
  * Hope you find it helpful, and get back if you have any feedback/improvements.
@@ -21,7 +21,7 @@
 (function($) {
 
 // Image template, this can be overridden from the constructor (options.template),
-// must contains {src} placeholder. Ther eare two class names 
+// must contains {src} placeholder. Ther eare two class names
 // 'chose-image' or 'chose-image-small', modifiy in CSS
 var fn_template = '<img class="{class_name}" src="{url}" />';
 
@@ -29,27 +29,27 @@ function prependTemplate(element, option, template, rtl, multiple, cls){
     // summery:
     // This will fill-in the provided template with image source and css class
     // then add it to the element.
-    // 
+    //
     // element: Dom
     //          This is the span node
-    // object: Object, 
+    // object: Object,
     //          The select option to get image url
     // template: String
     //          Html content
-    // rtl: Boolean, 
+    // rtl: Boolean,
     //          Right to Left
     // multiple: Boolean
     //          Used to select the approperiate css class, default to 'true'
     // cls*: String (optional)
     //          Css styles class
-    
+
     var src = $(option).data('img-src');
 
     if(src != undefined && src > ''){
 
         element = $(element);
 
-        text     = $(option).val();
+        text     = $(option).text();
         multiple = (multiple != undefined) ? multiple : true;
         cls      = cls || (multiple ? 'chose-image' : 'chose-image-small');
         cls      = rtl ? cls + ' rtl' : cls;
@@ -59,7 +59,7 @@ function prependTemplate(element, option, template, rtl, multiple, cls){
 
         // Empty the element
         element.empty();
-        
+
         // Insert after if ltr or multiple select, otherwise, insert before
         if(rtl && multiple){
             element.append(template);
@@ -72,12 +72,12 @@ function prependTemplate(element, option, template, rtl, multiple, cls){
 
 function getSelectedOptions(chosen){
     // summery:
-    //      Return a list of selected items/options with the selected 'option' elements 
+    //      Return a list of selected items/options with the selected 'option' elements
     //      and chosen 'span' elements.
-    // 
+    //
     // chosen: Chosen
     //          The Chosen object
-    
+
     var items    = [];
     var options  = $(chosen.form_field).find('option:selected') || [];
     var spans    = (chosen.is_multiple) ? $(chosen.container).find('.chosen-choices span'):
@@ -104,9 +104,9 @@ $.fn.extend({
     // Original behavior - use function.apply to preserve context
     var ret = fn_chosen.apply(this, arguments);
 
-    // Process all select elements to attach event handlers 
+    // Process all select elements to attach event handlers
     // (change, hiding_dropdown and showing_dropdown)
-    
+
     this.each(function() {
 
         var $this = $(this), chosen = $this.data('chosen');
@@ -123,7 +123,7 @@ $.fn.extend({
             var options       = getSelectedOptions(chosen.chosen);
             var rtl           = chosen.chosen.is_rtl;
             var multiple      = chosen.chosen.is_multiple;
-            var html_template = params.html_template || 
+            var html_template = params.html_template ||
                                 (rtl && multiple ? '{text}' + fn_template : fn_template + '{text}');
 
             for (var i = 0; i < options.length; i++){
@@ -133,7 +133,7 @@ $.fn.extend({
 
         $this.on("chosen:showing_dropdown", function showing_dropdown(evt, chosen){
             // summery
-            //    This function is triggered when the chosen instance dropdown list 
+            //    This function is triggered when the chosen instance dropdown list
             //    becomes visible. http://forwebonly.com/jquery-chosen-custom-events-and-how-to-use-them/
             //
             // evt: Event
@@ -144,11 +144,11 @@ $.fn.extend({
             var chosen        = chosen.chosen;
             var options       = chosen.form_field.options || [];
             var rtl           = chosen.is_rtl;
-            var html_template = params.html_template || 
+            var html_template = params.html_template ||
                                 rtl ? '{text}'+fn_template : fn_template+'{text}';
 
             var lis = $(chosen.container).find('.chosen-drop ul li:not(:has(img))')
-            
+
             for(var i = 0; i < lis.length; i++){
                 var li      = lis[i];
                 var option  = $(options[i]);
@@ -167,7 +167,7 @@ $.fn.extend({
             //  This never triggers. TODO: remove.
             $this.trigger('chosen:hiding_dropdown',chosen);
         });
-      
+
         $this.on('chosen:filter', function(evt, chosen){
             // summery;
             //  Support search, pending: https://github.com/harvesthq/chosen/pull/2373
